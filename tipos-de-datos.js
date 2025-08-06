16aa7f55b73ecad25597108463c7c37cb62cf4e8
alezvi <info@alezvi.com.ar>
2025-08-06 19:11:15 -0300

    sintaxis de js y tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,20 @@
+// tipos de datos en variables
+
+// undefined
+var x;
+var x = undefined;
+
+// number
+var n = 123;   // entero
+var t = 35.8;  // decimal
+
+// string (cadena de texto o cadena de caracteres)
+// entre comillas es un string sin importar el contenido
+var a = ''; // texto vacio, de longitud 0
+var b = 'hola mundo'; // de longitud 10
+var c = '123';
+var d = "comillas dobles"; // es igual el tipo de comilla
+
+// boolean - solo admite dos valores
+var verdad  = true;  // un 1 puede ser interpretado como true 
+var mentira = false; // un 0 puede ser interpreado como false
